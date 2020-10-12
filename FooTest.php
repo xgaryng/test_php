7586082ef9fe39dc6dd3370fc8c02567621049de
xingobar <gary840227@gmail.com>
2020-10-12 12:04:01 +0800

    first commit

---
+++ FooTest.php
@@ -0,0 +1,17 @@
+<?php
+class FooTest extends PHPUnit\Framework\TestCase
+{
+    public function test1()
+    {
+       $this->assertEquals(1, 1); 
+    }
+    
+    /* 如果你觉得一个case太单薄，可以放开此处
+    public function test2()
+    {
+       $this->assertEquals(1, 1); 
+       $this->assertEquals(1, 1); 
+    }
+    */
+}
+
