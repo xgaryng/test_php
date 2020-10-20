62734b630bbf1d8c8a213c63be8fc9014be3789b
xingobar <gary840227@gmail.com>
2020-10-20 22:12:26 +0800

    create footwo

---
+++ src/FooTwo.php
@@ -0,0 +1,21 @@
+<?php
+class FooTwo
+{
+
+
+    public function test1()
+    {
+        echo 'test';
+        echo 'test';
+    //   $this->assertEquals(1, 1); 
+    }
+    
+    /* 如果你觉得一个case太单薄，可以放开此处
+    public function test2()
+    {
+       $this->assertEquals(1, 1); 
+       $this->assertEquals(1, 1); 
+    }
+    */
+}
+
